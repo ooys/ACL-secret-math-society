dd97cbae892322f345e8182db8550d59bc23aed3
Yihong Song <1834395537@qq.com>
2021-02-10 03:13:26 -0500

    Add Smooth Scrolling, Start Footer

---
+++ components/footer.js
@@ -0,0 +1,22 @@
+
+function Footer(){
+    return(
+        <div>
+            <footer className="footer">
+                <div className="columns" >
+                    <div className="column">
+                        Test
+                    </div>
+                    <div className="column">
+                        Test2
+                    </div>
+                    <div className="column">
+                        Test3
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
